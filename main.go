6edfac937fd63631a83364888830fc33a49ffb9f
Me <TotallyNotLost@proton.me>
2025-04-02 19:02:12 +0000

    feat: Add escape support

--- main.go
+++ main.go
@@ -3,10 +3,105 @@ package main
 import "os"
 import tea "github.com/charmbracelet/bubbletea"
 import "github.com/charmbracelet/bubbles/list"
+import "github.com/charmbracelet/bubbles/viewport"
+import "github.com/charmbracelet/lipgloss"
 import "github.com/samber/lo"
 import "log"
 import "regexp"
 import "strings"
+
+type item struct {
+	title, content, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+var mainStyle = lipgloss.NewStyle().
+		MarginLeft(2).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62")).
+		PaddingRight(2)
+var pageStyle = lipgloss.NewStyle()
+
+type viewportModel struct {
+	viewport viewport.Model
+	list list.Model
+	Chosen bool
+}
+
+func (model viewportModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m viewportModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "esc":
+			m.Chosen = false
+			return m, nil
+		case "enter":
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				e, _ := newExample(i)
+				m.viewport.SetContent(pageStyle.Render(e.View()))
+				m.Chosen = true
+				return m, nil
+			}
+			return m, tea.Quit
+		default:
+			var cmd tea.Cmd
+			if m.Chosen {
+				m.viewport, cmd = m.viewport.Update(msg)
+				return m, cmd
+			}
+			m.list, cmd = m.list.Update(msg)
+			return m, cmd
+		}
+	case tea.WindowSizeMsg:
+		if !m.Chosen {
+			h, v := mainStyle.GetFrameSize()
+			m.list.SetSize(msg.Width-h, msg.Height-v)
+			m.list.SetSize(78, 40)
+		}
+	}
+	return m, nil
+}
+
+func (m viewportModel) View() string {
+	if !m.Chosen {
+		m.viewport.SetContent(pageStyle.Render(m.list.View()))
+	} else {
+	}
+
+	return mainStyle.Render(m.viewport.View())
+}
+
+func newList() list.Model {
+	items := []list.Item{}
+
+	notes := strings.SplitSeq(ReadFile(os.Args[1]), "\n---\n")
+
+	for note := range notes {
+		title := strings.Split(normalize(note), "\n")[0]
+		metadata := getMetadata(note)
+		var itm item
+		itm = item{title: title, content: note}
+		if tags, ok := metadata["tags"]; ok {
+			itm.desc = tags
+		}
+		items = append(items, itm)
+	}
+
+	l := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	l.Title = os.Args[1]
+
+	return l
+}
 
 func main() {
 	items := []list.Item{}
@@ -24,10 +119,13 @@ func main() {
 		items = append(items, itm)
 	}
 
-	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
-	m.list.Title = os.Args[1]
+	const width = 78
 
-	p := tea.NewProgram(m, tea.WithAltScreen())
+	vp := viewport.New(width, 40)
+
+	l := newList()
+
+	p := tea.NewProgram(&viewportModel{ viewport: vp, list: l }, tea.WithAltScreen())
 
 	if _, err := p.Run(); err != nil {
 		panic(err)
--- view.go
+++ view.go
@@ -13,39 +13,19 @@ func (e example) Init() tea.Cmd {
 	return nil
 }
 
-func (e example) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c", "esc":
-			return e, tea.Quit
-		default:
-			var cmd tea.Cmd
-			e.viewport, cmd = e.viewport.Update(msg)
-			return e, cmd
-		}
-	default:
-		return e, nil
-	}
-}
-
 func (e example) View() string {
 	return e.viewport.View() + e.helpView()
 }
 
 var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
 func (e example) helpView() string {
-	return helpStyle("\n  ↑/↓: Navigate • q: Quit\n")
+	return helpStyle("\n  ↑/↓: Navigate • esc: Back • q: Quit\n")
 }
 
 func newExample(i item) (*example, error) {
 	const width = 78
 
 	vp := viewport.New(width, 20)
-	vp.Style = lipgloss.NewStyle().
-		BorderStyle(lipgloss.RoundedBorder()).
-		BorderForeground(lipgloss.Color("62")).
-		PaddingRight(2)
 
 	// We need to adjust the width of the glamour render from our main width
 	// to account for a few things:
